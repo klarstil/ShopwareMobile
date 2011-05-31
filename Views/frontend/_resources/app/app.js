19f0daa5b20ae29761aaceeb308779fca28cd0d0
s.pohl <s.pohl@a972e975-1290-4f48-91ee-8f8a1ad0e57b>
2011-05-31 15:04:54 +0000

    #5523 - smaller image on cart view for better usability

--- Views/frontend/_resources/app/app.js
+++ Views/frontend/_resources/app/app.js
@@ -7,6 +7,7 @@
  * ----------------------------------------------------------------------
  */
 
+
 Ext.ns('App.views.Viewport', 'App.views.Shop', 'App.views.Search', 'App.views.Cart', 'App.views.Account', 'App.views.Info', 'App.views.Checkout');
 
 /** @private */
